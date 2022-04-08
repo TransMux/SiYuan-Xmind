f8d298ae59192acd14d44821dbb2fb74fe514ff3
InEase <InEase28@gmail.com>
2022-04-08 11:42:32 +0800

    docs: 添加注释

--- src/lib/M3Creator.ts
+++ src/lib/M3Creator.ts
@@ -1,7 +1,9 @@
 import { parseXMindMarkToXMindFile } from 'xmindmark'
 import save from 'save-file'
 import { listDocsByPath, getDocOutline, DocOutline } from './SiYuan'
+import { resolve } from 'path'
 
+// m3字符串的格式大概就是这样的
 const xmindMarkFileContent = `
 Central Topic
 - Main Topic 1
@@ -12,31 +14,43 @@ const indent = "    "
 
 
 export async function CreateM3(center: string, notebook: string, path: string) {
+  // 入口函数，开始构建大纲列表
   let result = `${center}\n` // 添加中心节点
   console.log("Create", center, notebook, path);
-  result += ListFile(notebook, path)
+  // 递归入口
+  await ListFile(notebook, path).then(
+    res => {
+      result += res
+    }
+  )
   console.log(result);
 }
 
-function ListFile(notebook: string, path: string, index = 0): string {
+async function ListFile(notebook: string, path: string, index = 0): Promise<string> {
   let result = ""
-  listDocsByPath(path, notebook).then(
+  // 列出当前目录下的全部文件
+  await listDocsByPath(path, notebook).then(
     res => {
-      res.files.forEach(file => {
+      res.files.forEach(async file => {
+        // 对于当前目录下的每个文件，先把标题加进来
         result += `${indent.repeat(index)}- ${file.name.replaceAll("&nbsp;", " ")}\n`
-        getDocOutline(file.id).then(
+        // 然后添加其大纲
+        await getDocOutline(file.id).then(
           res => {
             if (res.length > 0) {
+              // 如果存在大纲，使用这个函数拼接大纲对应的m3字符串
               result += ExtractOutline(res[0].blocks, index + 1)
-              // console.log(result);
+              console.log(result);
             }
           })
 
-        // 查找子文件
+        // 如果当前文件下面存在子文件
         if (file.subFileCount > 0) {
-          result += ListFile(notebook, file.path, index + 1)
-          // console.log(result);
-
+          // 就递归调用自己，添加下一级的大纲
+          await ListFile(notebook, file.path, index + 1).then(
+            res => result += res
+          )
+          console.log(result);
         }
       });
     }
@@ -50,8 +64,10 @@ function ExtractOutline(outlines: DocOutline[] | undefined | null, index: number
   }
   let result = ""
   outlines.forEach(outline => {
+    // 替换标题中的空格
     let content = outline.content.replaceAll("&nbsp;", " ")
     result += `${indent.repeat(index)}- ${content}\n`
+    // 然后加进来，如果这个标题下面有小标题，那么递归调用也把它加进来
     result += ExtractOutline(outline.children, index + 1)
   }
   )
