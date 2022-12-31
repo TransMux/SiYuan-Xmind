2d1bdfc829c228de37314781ee13cec7172365b9
OpaqueGlass <8145162+opaqueglass@user.noreply.gitee.com>
2022-12-31 09:44:10 +0800

    模式选择&大纲层级范围选择
    Squashed commit:
    
    [786f9e2] fix: dccc26引入的index+1错误
    
    [6aedc20] enhance: 大纲范围选择，调整页面
    fix: ？？？ 8ed329引入的await/async请求顺序错误；
    
    [8ed3290] fix: 修复第二次刷新不生效的问题
    enhance: 用一种及其糟糕的方式实现方式选择
    
    [0872096] fix: 大纲缺少第一层级的问题 enhance: 无子文档时输出大纲

--- src/lib/M3Creator.ts
+++ src/lib/M3Creator.ts
@@ -10,42 +10,83 @@ Central Topic
 `
 
 const indent = "    "
-
-
-export async function ListFile(notebook: string, path: string, index = 0): Promise<string> {
+export let outlineScope: Array<any> = [1, 6];
+/**
+ * 
+ * @param notebook 
+ * @param path 
+ * @param mode this_doc_only 或 with_child_doc 或 with_child_doc_outline
+ * @param index 
+ * @returns 
+ */
+export async function ListFile(notebook: string, path: string, mode:string, index = 0): Promise<string> {
   let result = ""
   // 列出当前目录下的全部文件
-  const docs = await listDocsByPath(path, notebook)
-  await docs.files.reduce(
-    // @ts-ignore TODO: reduce 此处TS会报错
-    async (memo, file) => {
-      await memo
-      result += `${indent.repeat(index)}- ${file.name.replaceAll("&nbsp;", " ")}\n`
-      const outline = await getDocOutline(file.id)
-      if (outline.length > 0) {
-        result += ExtractOutline(outline[0].blocks, index + 1)
-      }
-
-      if (file.subFileCount > 0) {
-        result += await ListFile(notebook, file.path, index + 1)
-      }
-    }, undefined
-  )
+  const docs:any = await listDocsByPath(path, notebook);
+  if (mode !== "this_doc_only") {
+    await docs.files.reduce(
+      async (memo:any, file:any) => {
+        await memo;
+        let tempFileName = file.name;
+        if (mode == "with_child_doc") tempFileName = tempFileName.substring(0, tempFileName.length - 3);
+        result += `${indent.repeat(index)}- ${tempFileName.replaceAll("&nbsp;", " ")}\n`;
+        if (mode == "with_child_doc_outline") {
+          const outline : any = await getDocOutline(file.id)
+          if (outline.length > 0) {
+            result += ExtractOutline(outline, index + 1)
+          }
+        }
+  
+        if (file.subFileCount > 0) {
+          result += await ListFile(notebook, file.path, mode, index + 1)
+        }
+      }, undefined
+    );
+  }
+  
+  if (result == "" && mode != "with_child_doc") {
+    let dividedPath = path.split("/");
+    let docid : any;
+    if (dividedPath.length > 0) {
+      docid = dividedPath[dividedPath.length - 1];
+      docid = docid.substring(0, docid.length - 3);
+    }
+    const outline : any = await getDocOutline(docid);
+    if (outline.length > 0) {
+      result += ExtractOutline(outline, index);
+    }
+  }
   return result
 }
 
-function ExtractOutline(outlines: DocOutline[] | undefined | null, index: number): string {
+function ExtractOutline(outlines: DocOutline[] | undefined | null, index: number, limit: Array<number> = outlineScope): string {
   if (!outlines) {
     return ""
   }
   let result = ""
-  outlines.map((outline => {
+  outlines.map((outline:any) => {
     // 替换标题中的空格
-    let content = outline.content.replaceAll("&nbsp;", " ")
-    result += `${indent.repeat(index)}- ${content}\n`
-    // 然后加进来，如果这个标题下面有小标题，那么递归调用也把它加进来
-    result += ExtractOutline(outline.children, index + 1)
-  }))
+    let content = outline.content;
+    // 获取两种情况下的大纲文本
+    if (content != null || content != undefined) {
+      content = content.replaceAll("&nbsp;", " ");
+    }else{
+      content = outline.name.replaceAll("&nbsp;", " ");
+    }
+    // 范围判断
+    let outlineHeadingNum = parseInt(outline.subType.substring(1, 2));
+    if (outlineHeadingNum >= limit[0]) {
+      result += `${indent.repeat(index)}- ${content}\n`;
+    }
+    // 下一层级大纲
+    if (outlineHeadingNum + 1 <= limit[1]) {
+      if (outline.type == "outline") {
+        result += ExtractOutline(outline.blocks, outlineHeadingNum >= limit[0]? index + 1 : index)
+      }else if (outline.type == "NodeHeading") {
+        result += ExtractOutline(outline.children, outlineHeadingNum >= limit[0]? index + 1 : index);
+      }
+    }
+  })
   return result
 }
 
